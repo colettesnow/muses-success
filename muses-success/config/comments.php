786a993f162cf7ebf78ae2dbdc5da9d3a7ed7e4d
Colette Snow <colette@colettesnow.com>
2014-08-26 13:45:43 +1000

    Add comments config file in prep. to add Disqus Comments

---
+++ muses-success/config/comments.php
@@ -0,0 +1,19 @@
+<?php
+
+// whether we should use native comments (false) or Disqus (true)
+$config["use_disqus"] = false;
+
+// Disqus Settings
+
+$config["use_disqus_sso"] = false;
+$config["disqus_secret_key"] = "";
+$config["disqus_public_key"] = "";
+
+$config["disqus_short_name"] = "";
+
+// Disqus Category IDs
+$config["disqus_listing_cat_id"] = "";
+$config["disqus_author_cat_id"] = "";
+$config["disqus_wiki_talk_page_cat_id"] = "";
+$config["disqus_user_comment_cat_id"] = "";
+$config["disqus_blog_cat_id"] = "";
