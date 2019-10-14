7a0fc32fddc877151ca83b315df24b8cc3d3469d
Colette Snow <colette@colettesnow.com>
2019-10-14 11:44:30 +1100

    Markup faqs as structured data

--- muses-success/views/pages/faqs.php
+++ muses-success/views/pages/faqs.php
@@ -1,3 +1,5 @@
+<div itemscope itemtype="https://schema.org/FAQPage" class="muse_content">
+
 <h1>Frequently Asked Questions</h1>
 
 <ol class="faqs faq-list">
@@ -16,10 +18,29 @@
 <h2 id="c-<?php echo $category['id']; ?>"><?php echo $category['id']; ?>. <?php echo $category['cat_name']; ?></h2>
 <?php foreach ($category['questions'] as $question): ?>
 <div class="faq">
-<h3 class="question" id="perma-<?php echo $question['perma-id']; ?>"><?php echo $category['id']; ?>.<?php echo $question['id']; ?>. <?php echo $question['question']; ?></h3>
-<div class="answer">
+<div itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
+<h3 class="question" id="perma-<?php echo $question['perma-id']; ?>" itemprop="name"><?php echo $category['id']; ?>.<?php echo $question['id']; ?>. <?php echo $question['question']; ?></h3>
+<div class="answer" itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+<div itemprop="text">
 <?php echo $question['answer']; ?>
 </div>
 </div>
+</div>
+</div>
 <?php endforeach; ?>
 <?php endforeach; ?>
+</div>
+
+<div class="ad">
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Muse's Success - Sidebar -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:160px;height:600px"
+     data-ad-client="ca-pub-4250402539296088"
+     data-ad-slot="1318512252"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</div>
+
+<div class="clear"></div>
